8ba0cb0c9bd7f8bc09e6f9ebe007f904105a12a3
koralina <kjedrzejak91@gmail.com>
2020-08-26 20:38:46 +0200

    add css, add function change bg

--- js/main.js
+++ js/main.js
@@ -7,35 +7,36 @@ const filter = document.querySelector('.filter');
 
 
 
+
+
+
 const addToDo = (e) => {
     e.preventDefault();
 
     const toDoItem = document.createElement('div');
     toDoItem.classList.add('itembox');
 
+    const itemNumber = document.getElementsByClassName("itembox");
+    const taskNumber = itemNumber.length
+    const countNumber = document.createElement('div');
+    countNumber.classList.add("task-number");
+    countNumber.innerText = `Task: ${taskNumber}`;
+    toDoItem.appendChild(countNumber);
+
+    const completeButton = document.createElement('button'); 
+    completeButton.classList.add('itembox__btn', 'itembox__btn--complete');
+    completeButton.innerHTML = "<i class='fas fa-check-circle'></i>";
+    toDoItem.appendChild(completeButton);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('itembox__btn', 'itembox__btn--delete');
+    deleteButton.innerHTML = "<i class='fas fa-minus-circle'></i>";
+    toDoItem.appendChild(deleteButton);
 
     const toDoNew = document.createElement('li');
     toDoNew.classList.add('itembox__item');
     toDoNew.innerText = toDoInput.value;
     toDoItem.appendChild(toDoNew);
-
-
-    const itemNumber = document.getElementsByClassName("itembox");
-    const taskNumber = itemNumber.length
-    const countNumber = document.createElement('div');
-    countNumber.classList.add("task-number");
-    countNumber.innerText = taskNumber + 1;
-    toDoItem.appendChild(countNumber);
-
-    const completeButton = document.createElement('button');
-    completeButton.classList.add('itembox__btn', 'itembox__btn--complete');
-    completeButton.innerText = "check";
-    toDoItem.appendChild(completeButton);
-
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('itembox__btn', 'itembox__btn--delete');
-    deleteButton.innerText = "delete";
-    toDoItem.appendChild(deleteButton);
 
     toDoList.prepend(toDoItem);
     toDoInput.value = '';
@@ -45,7 +46,6 @@ const addToDo = (e) => {
 
 const deleteAndCheck = (e) =>{
     const btn = e.target;
-    console.log(btn);
 
     if(btn.classList[1] === 'itembox__btn--complete'){
         const item = btn.parentElement;
@@ -69,25 +69,47 @@ function selectToDo(e) {
 
         if(e.target.value === "completed"){
             if(element.classList.contains("completed")){
-                    element.style.display = "block";
+                    element.style.display = "flex";
                 } else {
                     element.style.display = "none";
                 }
             }
         else if(e.target.value === "uncompleted") {
                 if(!element.classList.contains("completed")){
-                    element.style.display = "block";
+                    element.style.display = "flex";
                 } else {
                     element.style.display = "none";
                 }
             }
         else {
-            element.style.display = "block";
+            element.style.display = "flex";
             }  
     });
-}   
-      
+}
 
+const changeBg = (e) => {
+    const body = document.querySelector('body');
+    const key = e.keyCode;
+    console.log(key);
+    
+
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    const r1 = Math.floor(Math.random()*256);
+    const g1 = Math.floor(Math.random()*256);
+    const b1 = Math.floor(Math.random()*256);
+
+    if (key === 32) {
+        console.log(body);
+
+        body.style.backgroundImage = `linear-gradient(360deg, rgb(${r},${g},${b}), rgb(${r1},${g1},${b1}))`;    
+    }
+
+   
+
+}
+      
 
 
 
@@ -96,3 +118,4 @@ function selectToDo(e) {
 toDoButton.addEventListener('click', addToDo);
 toDoList.addEventListener('click', deleteAndCheck);
 filter.addEventListener('click', selectToDo);
+window.addEventListener('keypress', changeBg);
